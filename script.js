c6cab937dc51431b222e4136b6b464a848bf14b2
Stephenna J Corry <Sjcstephenna@gmail.com>
2019-03-20 01:08:16 -0400

    score now accurately registering with button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score"); 
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
@@ -20,10 +20,12 @@ function getComputerChoice(){
 function win(){
     userScore++;
     userScore_span.innerHTML = userScore;
-    computerScore_span = computerScore;
+    computerScore_span.innerHTML = computerScore;
 }
 function lose(){
-
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
 }
 
 function draw(){
